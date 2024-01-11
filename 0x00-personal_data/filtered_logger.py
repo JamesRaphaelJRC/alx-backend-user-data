dd9ad6b8cb51a7e138f9af57bdde750b1a2ab2a4
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-01-11 16:51:08 +0100

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
-''' Defines a function filter_datum '''
-import re
+"""
+Module for handling Personal Data
+"""
 from typing import List
+import re
+import logging
+from os import environ
+# import mysql.connector
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str):
-    '''
-    Returns the log message obfuscated
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
-    Arguments:
-    fields: a list of strings representing all fields to obfuscate
-    redaction: a string representing by what the field will be obfuscated
-    message: a string representing the log line
-    separator: a string representing by which character is separating all
-                fields in the log line (message)
-    '''
-    for data in fields:
-        message = re.sub(f'{data}=.*?{separator}',
-                         f'{data}={redaction}{separator}', message)
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
     return message
