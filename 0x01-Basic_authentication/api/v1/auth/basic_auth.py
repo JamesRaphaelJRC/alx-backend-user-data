4b5829590379bd11c54c02ac7104f3569befc594
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-01-17 16:13:22 +0100

    task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 '''
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -44,3 +45,31 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(':')
         return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        ''' Returns the User instance based on email and password
+        and returns None if there is no User instance with given user_email or
+        user_pwd is invalid
+        '''
+        if user_email is None or user_pwd is None:
+            return None
+        if type(user_email) != str or type(user_pwd) != str:
+            return None
+        from models.user import User
+
+        User.load_from_file()
+        from models.base import DATA
+        if len(DATA) == 0:
+            return None
+        users = DATA.get('User')
+
+        # Check if a User instance with email == user_email exists
+        user_instance = [user for user in users.values() if user.search(
+            {'email': user_email}) != [] and user.is_valid_password(user_pwd)]
+
+        # Check if no instance is found
+        if len(user_instance) == 0:
+            return None
+        else:
+            return user_instance[0]
