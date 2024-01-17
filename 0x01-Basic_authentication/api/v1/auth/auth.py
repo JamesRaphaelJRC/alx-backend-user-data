19a063bed20cb4bf400ebcbe4bd61cc8aef27607
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-01-17 10:58:16 +0100

    task 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -41,9 +41,9 @@ class Auth:
             Value of the request header 'Authorization' if
             'Authorization' is in the request object and None otherwise
         '''
-        if request is None:
+        if request is None or 'Authorization' not in request.headers:
             return None
-        return request.headers('Authorization', None)
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         ''' Returns None
