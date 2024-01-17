e5fb079b647e35228f2b7ff3792b957cfdf852ec
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-01-17 11:14:41 +0100

    task 6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,11 +18,14 @@ AUTH_TYPE = getenv("AUTH_TYPE")
 if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif AUTH_TYPE == 'BasicAuth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
 def before_request() -> str:
-    ''' Handles basic authentication
+    ''' Handles before request
     '''
     if auth is not None:
         excluded_paths = ['/api/v1/status/',
