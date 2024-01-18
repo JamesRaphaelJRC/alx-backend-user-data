b7dc0f74bc2a42f72d97f80be7e9095277c1349a
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-01-18 13:30:17 +0100

    require_auth allows *

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -27,10 +27,17 @@ class Auth:
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
 
-        if path[-1] != '/':
-            path += '/'
-        if path in excluded_paths:
-            return False
+        path = path.rstrip('/')  # Removes all '/' from path before comparison
+
+        for excluded_path in excluded_paths:
+            # Remove all '/' from excluded_path before comparison with path
+            excluded_path = excluded_path.rstrip('/')
+
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):  # Excludes '*'
+                    return False
+            elif path == excluded_path:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
