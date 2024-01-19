654a200970e7860bd447a915841789cc866c835c
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-01-19 15:28:36 +0100

    task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -26,3 +26,12 @@ class SessionAuth(Auth):
         if session_id is None and type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        ''' Returns a User instance based on a cookie
+        '''
+        from models.user import User
+
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_by_session_id.get(session_id)
+        return User.get(user_id)
