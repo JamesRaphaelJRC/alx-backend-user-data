31e3ad289e5f880d5b7fa070083edf9b6fb95e6b
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-01-19 14:27:02 +0100

    task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,7 +15,14 @@ class SessionAuth(Auth):
         '''
         if user_id is None or type(user_id) != str:
             return None
-        s_id = str(uuid4())
-        self.user_id_by_session_id[s_id] = user_id
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
 
-        return s_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        ''' Return a User ID based on a Session ID
+        '''
+        if session_id is None and type(session_id) != str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
