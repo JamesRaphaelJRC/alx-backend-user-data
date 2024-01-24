f8acf63aaa922439f3ee08210819a7b37cf00855
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-01-24 14:17:57 +0100

    task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,3 +53,19 @@ class Auth:
             return True
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        ''' Generates a session_id for a given user
+        Return:
+            session_id - on success
+            None - otherwise
+        '''
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+
+            user.session_id = session_id
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
