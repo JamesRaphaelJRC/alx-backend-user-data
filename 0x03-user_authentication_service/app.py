09b85594be8ca28151599d85f38cfe3df95a89db
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-01-24 16:05:58 +0100

    task 15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -78,7 +78,7 @@ def profile() -> str:
         User credentials
     '''
     session_id = request.cookies.get('session_id')
-    if session is None:
+    if session_id is None:
         abort(403)
 
     user = AUTH.get_user_from_session_id(session_id)
